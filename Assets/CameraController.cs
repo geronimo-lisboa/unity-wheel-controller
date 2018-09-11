d1980279147f9ecc3e76752a4407043473ad6cf1
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-09-10 22:26:16 -0300

    Breaks working.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        Vector3 targetPos = target.position;
+        Vector3 targetForward = target.forward;
+        Vector3 camPos = targetPos + targetForward * (-7.0f) + new Vector3(0, 4.0f, 0);
+        this.transform.position = camPos;
+        this.transform.LookAt(target);
+    }
+}
---
+++ Assets/SimpleCarController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?    
+}
+
+public class SimpleCarController : MonoBehaviour {
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    
+
+    public void FixedUpdate()
+    {
+        //Pega o motor e a direção de acordo com o input
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        //Pra ver se tem que freiar
+        float breakTorque = 0;
+        if(Input.GetKey(KeyCode.Space))
+        {
+            breakTorque = 300000;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            breakTorque = 0;
+        }
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;                
+                axleInfo.rightWheel.steerAngle = steering;
+                axleInfo.leftWheel.brakeTorque = breakTorque;
+                axleInfo.rightWheel.brakeTorque = breakTorque;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+                axleInfo.leftWheel.brakeTorque = breakTorque;
+                axleInfo.rightWheel.brakeTorque = breakTorque;
+            }
+        }
+        Debug.Log("Motor: " + motor);
+    }
+}
